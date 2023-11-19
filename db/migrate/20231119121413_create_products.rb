b6c77ddc9eafa9afab6bc6abbc93120a952d7c5b
charlie morris <charliemorris.96@gmail.com>
2023-11-19 12:31:21 +0000

    model setup

---
+++ db/migrate/20231119121413_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.decimal :price
+      t.integer :vintage
+      t.integer :quantity
+      t.references :category_id, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
