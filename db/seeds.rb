a5c1e0bf5e7f5908acb97dafab465dbc802c4314
charlie morris <charliemorris.96@gmail.com>
2023-11-19 13:34:49 +0000

    seed file created

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,36 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# Seed categories
+Category.create(name: 'Category 1')
+Category.create(name: 'Category 2')
+# Add more categories as needed...
+
+# Seed users
+User.create(email: 'user1@example.com', encrypted_password: 'password123')
+User.create(email: 'user2@example.com', encrypted_password: 'password456')
+# Add more users as needed...
+
+# Seed products
+Product.create(name: 'Product 1', description: 'Description for Product 1', price: 19.99, vintage: 2022, quantity: 10, category_id: 1)
+Product.create(name: 'Product 2', description: 'Description for Product 2', price: 29.99, vintage: 2021, quantity: 5, category_id: 2)
+# Add more products as needed...
+
+# Seed addresses
+Address.create(user_id: 1, address_line: '123 Street', city: 'City', county: 'County', post_code: '12345')
+Address.create(user_id: 2, address_line: '456 Street', city: 'City', county: 'County', post_code: '67890')
+# Add more addresses as needed...
+
+# Seed orders
+Order.create(total_price: 39.99, user_id: 1, product_id: 1)
+Order.create(total_price: 59.99, user_id: 2, product_id: 2)
+# Add more orders as needed...
+
+# Seed payments
+Payment.create(order_id: 1, transaction_id: 'ABC123', amount: 39.99, payment_method: 'Credit Card')
+Payment.create(order_id: 2, transaction_id: 'DEF456', amount: 59.99, payment_method: 'PayPal')
+# Add more payments as needed...
+
+# Seed reviews
+Review.create(user_id: 1, product_id: 1, rating: 5, description: 'Great product!')
+Review.create(user_id: 2, product_id: 2, rating: 4, description: 'Nice item.')
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -4,4 +4,10 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "should create product" do
+    assert_difference('Product.count') do
+      post products_url, params: { product: { name: 'Sample Product', price: 10.99 } }
+    end
+    assert_redirected_to product_url(Product.last)
+  end
 end
