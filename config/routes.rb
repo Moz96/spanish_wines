b8b19f6650d78fed576dd22226d01404ef996c0a
charlie morris <charliemorris.96@gmail.com>
2023-11-20 19:29:10 +0000

    Added Cart and CartItem models

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   root 'home#index'
   resources :products do
-    resources :reviews
+    resources :orders, only:[:new, :create, :show]
+    resources :reviews, only:[:new, :create, :show]
   end
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_19_142822) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_20_192825) do
   create_table "addresses", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "address_line"
@@ -20,6 +20,23 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_19_142822) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "cart_items", force: :cascade do |t|
+    t.integer "cart_id", null: false
+    t.integer "product_id", null: false
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
+    t.index ["product_id"], name: "index_cart_items_on_product_id"
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -77,6 +94,9 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_19_142822) do
   end
 
   add_foreign_key "addresses", "users"
+  add_foreign_key "cart_items", "carts"
+  add_foreign_key "cart_items", "products"
+  add_foreign_key "carts", "users"
   add_foreign_key "orders", "products"
   add_foreign_key "orders", "users"
   add_foreign_key "payments", "orders"
