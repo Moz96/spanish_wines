cc5602e83753677a9caf8650970700eb3d7e2ea0
charlie morris <charliemorris.96@gmail.com>
2023-11-22 11:56:23 +0000

    about page created

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   root 'home#index'
+  get 'about', to: 'about#index'
   resources :products do
     resources :orders, only:[:new, :create, :show]
     resources :reviews, only:[:new, :create, :show]
