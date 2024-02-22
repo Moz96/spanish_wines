3fe2a8f2ba05718ce60fdc9c58f9fe20aaf5cc8a
charlie morris <charliemorris.96@gmail.com>
2024-02-22 10:58:15 +0000

    disable add button when quantity reached

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,5 @@ class Product < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :carts, through: :cart_items
   has_one_attached :image, dependent: :destroy
+  validates :quantity, numericality: { greater_than_or_equal_to: 0 }
 end
